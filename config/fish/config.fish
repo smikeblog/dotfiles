f019f8f9227d1bdc0b14e57cd6c364bf703cacd4
Smike <smikemail@mail.ru>
2019-09-20 04:55:47 -0400

    made changes to config.fish sxhkdrc and tmux.conf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -32,7 +32,7 @@ function ..    ; cd .. ; end
 function ...   ; cd ../.. ; end
 function ....  ; cd ../../.. ; end
 function ..... ; cd ../../../.. ; end
-function ll    ; tree --dirsfirst -ChFupDaLg 1 $argv ; end
+function llt    ; tree --dirsfirst -ChFupDaLg 1 $argv ; end
 
 # Utilities
 function a        ; command ag --ignore=.git --ignore=log --ignore=tags --ignore=tmp --ignore=vendor --ignore=spec/vcr $argv ; end
@@ -155,3 +155,102 @@ end
 
 # iterm2
 # test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+if command -v exa > /dev/null
+	abbr -a lx 'exa'
+	abbr -a ls 'exa'
+	abbr -a ll 'exa -l'
+	abbr -a lll 'exa -la'
+else
+	abbr -a l 'ls'
+	abbr -a ll 'ls -l'
+	abbr -a lll 'ls -la'
+end
+
+function fish_greeting
+	echo
+	echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e " \\e[1mDisk usage:\\e[0m"
+	echo
+	echo -ne (\
+		df -l -h | grep -E 'dev/(xvda|sd|mapper)' | \
+		awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
+		sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
+		paste -sd ''\
+	)
+	echo
+
+	echo -e " \\e[1mNetwork:\\e[0m"
+	echo
+	# http://tdt.rocks/linux_network_interface_naming.html
+	echo -ne (\
+		ip addr show up scope global | \
+			grep -E ': <|inet' | \
+			sed \
+				-e 's/^[[:digit:]]\+: //' \
+				-e 's/: <.*//' \
+				-e 's/.*inet[[:digit:]]* //' \
+				-e 's/\/.*//'| \
+			awk 'BEGIN {i=""} /\.|:/ {print i" "$0"\\\n"; next} // {i = $0}' | \
+			sort | \
+			column -t -R1 | \
+			# public addresses are underlined for visibility \
+			sed 's/ \([^ ]\+\)$/ \\\e[4m\1/' | \
+			# private addresses are not \
+			sed 's/m\(\(10\.\|172\.\(1[6-9]\|2[0-9]\|3[01]\)\|192\.168\.\).*\)/m\\\e[24m\1/' | \
+			# unknown interfaces are cyan \
+			sed 's/^\( *[^ ]\+\)/\\\e[36m\1/' | \
+			# ethernet interfaces are normal \
+			sed 's/\(\(en\|em\|eth\)[^ ]* .*\)/\\\e[39m\1/' | \
+			# wireless interfaces are purple \
+			sed 's/\(wl[^ ]* .*\)/\\\e[35m\1/' | \
+			# wwan interfaces are yellow \
+			sed 's/\(ww[^ ]* .*\).*/\\\e[33m\1/' | \
+			sed 's/$/\\\e[0m/' | \
+			sed 's/^/\t/' \
+		)
+	echo
+
+	set r (random 0 100)
+	if [ $r -lt 5 ] # only occasionally show backlog (5%)
+		echo -e " \e[1mBacklog\e[0;32m"
+		set_color blue
+		echo "  [project] <description>"
+		echo
+	end
+
+	set_color normal
+	echo -e " \e[1mTODOs\e[0;32m"
+	echo
+	if [ $r -lt 10 ]
+		# unimportant, so show rarely
+		set_color cyan
+		# echo "  [project] <description>"
+	end
+	if [ $r -lt 25 ]
+		# back-of-my-mind, so show occasionally
+		set_color green
+		# echo "  [project] <description>"
+	end
+	if [ $r -lt 50 ]
+		# upcoming, so prompt regularly
+		set_color yellow
+		# echo "  [project] <description>"
+	end
+
+	# urgent, so prompt always
+	set_color red
+	# echo "  [project] <description>"
+
+	echo
+
+	if test -s ~/todo
+		set_color magenta
+		cat todo | sed 's/^/ /'
+		echo
+	end
+
+	set_color normal
+end
