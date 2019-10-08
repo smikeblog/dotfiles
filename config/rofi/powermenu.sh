276e7970fa60171344dc8dd1d57d47ebc5cf2f5c
Smike <smikemail@mail.ru>
2019-10-08 12:44:16 +0300

    added rofi config and mod on polybar config

---
+++ config/rofi/powermenu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+rofi_command="rofi -theme powermenu.rasi"
+
+### Options ###
+power_off="襤"
+reboot="ﰇ"
+suspend="鈴"
+log_out=""
+# Variable passed to rofi
+options="$power_off\n$reboot\n$suspend\n$log_out"
+
+chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 2)"
+case $chosen in
+    $power_off)
+        systemctl poweroff
+        ;;
+    $reboot)
+        systemctl reboot
+        ;;
+    $suspend)
+        mpc -q pause
+        amixer set Master mute
+        systemctl suspend
+        ;;
+    $log_out)
+        bspc quit
+        ;;
+esac
+
