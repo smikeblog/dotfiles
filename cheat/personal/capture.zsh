94d77f2b102a393e14b94a326532d1ff17e9a637
smikeblog <smikeya@ya.ru>
2020-11-07 23:45:05 +0200

    some cheats + syscheck(s) + zsh fix

---
+++ cheat/personal/capture.zsh
@@ -0,0 +1,13 @@
+
+# Usage:
+  capture.zsh bufferstring
+
+# Examples:
+
+  capture.zsh 'vim -'
+  capture.zsh 'vim --'
+  capture.zsh 'vim --r'
+  capture.zsh 'echo *('
+  capture.zsh 'scp hostname:'
+
+NOTE: from https://github.com/Valodim/zsh-capture-completion
