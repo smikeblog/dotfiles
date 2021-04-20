1105242474e18b4b5ae1aa9985f893d299b46ec6
smikeblog <smikemail@mail.ru>
2021-04-20 11:38:38 +0300

    pet && procs moved to zinit-managed && added zinit-managed-procs.md

---
+++ zinit/zinit-manageg-progs.md
@@ -0,0 +1,29 @@
+# Programs managed by zinit setup
+
+01. neovim as appimage
+02. lazygit
+03. forgit
+04. prettyping aliased to ping
+05. xurls
+06. mmv
+07. pet
+08. navi
+09. tree-sitter
+10. micro
+11. nnn
+12. lf
+13. exa
+14. emojify
+15. rg
+16. k
+17. exiftool
+18. anyframe
+19. bat && bat-extras(batgrep; batdiff)
+20. fd
+21. lsd
+22. glow
+23. fzf
+24. fzf-tab
+25. delta
+26. procs
+
---
+++ zsh-plug-funcs/pet_atload.zsh
@@ -0,0 +1,18 @@
+function prev() {
+  PREV=$(fc -lrn | head -n 1)
+  sh -c "pet new `printf %q "$PREV"`"
+
+}
+
+function pet-select() {
+  BUFFER=$(pet search --query "$LBUFFER")
+  CURSOR=$#BUFFER
+  zle redisplay
+
+}
+zle -N pet-select
+# move pluginlist because async load occurs below error
+# # stty: 'standard input': Inappropriate ioctl for device
+# # [[ $- == *i*  ]] && stty -ixon
+# bindkey '^s' pet-select
+#
