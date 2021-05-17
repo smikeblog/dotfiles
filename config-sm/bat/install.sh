73beb6e1edae6a3118a2371f1e139fbbf9f5c888
smikeblog <smikemail@mail.ru>
2021-05-17 20:19:02 +0300

    bat extras removed from dotfiles & added to zinit

--- config-sm/bat/install.sh
+++ config-sm/bat/install.sh
@@ -6,13 +6,6 @@ source "$(cd -P "$(dirname "$0")" && pwd -P)/../base.sh"
 
 do_install() {
     link_file "$src_conf" "$HOME/.config/bat"
-
-    link_file "$src_conf/bat-extras-src/batman.sh" "$HOME/bin/batman"
-    link_file "$src_conf/bat-extras-src/batdiff.sh" "$HOME/bin/batdiff"
-    link_file "$src_conf/bat-extras-src/batgrep.sh" "$HOME/bin/batgrep"
-    link_file "$src_conf/bat-extras-src/prettybat.sh" "$HOME/bin/batpretty"
-    link_file "$src_conf/bat-extras-src/bat-modules.sh" "$HOME/bin/bat-modules"
-    link_file "$src_conf/bat-extras-src/batwatch.sh" "$HOME/bin/batwatch"
 }
 
 install
