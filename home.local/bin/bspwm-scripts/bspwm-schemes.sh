d2858029e2598df3d678406dcd94f763133a5d17
smikeblog <smikemail@mail.ru>
2021-06-15 17:41:35 +0300

    added bspwm mode set -binded to super+[a,s,w]

--- home.local/bin/bspwm-scripts/bspwm-schemes.sh
+++ home.local/bin/bspwm-scripts/bspwm-schemes.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-if [ "$1" == "first_child" ]
+if [ "$1" == "alternate" ]
 then
+  notify-send -i alternate "Switched to alternate mode!" && \
   bspc config automatic_scheme first_child
-  zenity --info --width=200 --height=100 --text "Switched to first_child mode!"
+#  zenity --info --width=200 --height=100 --text "Switched to first_child mode!"
   exit 0
 elif [ "$1" == "longest_side" ]
 then
+  notify-send -i longside  "Switched to longest_side mode!" && \
   bspc config automatic_scheme longest_side
-  zenity --info --width=200 --height=100 --text "Switched to longest_side mode!"
+#  zenity --info --width=200 --height=100 --text "Switched to longest_side mode!"
   exit 0
 elif [ "$1" == "spiral" ]
 then
+  notify-send -i spiral "Switched to spiral mode!" && \
   bspc config automatic_scheme spiral
-  zenity --info --width=200 --height=100 --text "Switched to spiral mode!"
+#  zenity --info --width=200 --height=100 --text "Switched to spiral mode!"
   exit 0
 else
-  zenity --info --width=200 --height=100 --text "Invalid mode!"
+  notify-send -i error "Invalid mode!"
+#  zenity --info --width=200 --height=100 --text "Invalid mode!"
   exit 1
 fi
